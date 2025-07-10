e16e99712f1416e17e96b1ed7776678144c5aa45
bayurespati <respatibayu48@gmail.com>
2025-07-10 15:03:50 +0700

    fix field plan

--- app/Http/Controllers/PlanController.php
+++ app/Http/Controllers/PlanController.php
@@ -26,8 +26,22 @@ class PlanController extends Controller
     //Get All
     public function getAll()
     {
-        $plan = Plan::with(['item_type', 'item_variety'])->get();
-        return response()->json(['data' => $plan, 'message' => 'Success get data plans'], 200);
+        $plans = Plan::with(['item_type', 'item_variety'])->get()->map(function ($plan) {
+            return [
+                'nama_barang' => $plan->nama_barang,
+                "uuid" => $plan->uuid,
+                "project_id" => $plan->project_id,
+                "judul" => $plan->judul,
+                "nama_barang" => $plan->nama_barang,
+                "jumlah_barang" => $plan->jumlah_barang,
+                "is_lop" => $plan->is_lop,
+                "file_prpo" => $plan->file_prpo,
+                "no_prpo" => $plan->no_prpo,
+                'jenis_barang_uuid' => optional($plan->item_variety)->uuid,
+                'tipe_barang_id' => optional($plan->item_type)->uuid,
+            ];
+        });
+        return response()->json(['data' => $plans, 'message' => 'Success get data plans'], 200);
     }
 
     //Get Garbage
