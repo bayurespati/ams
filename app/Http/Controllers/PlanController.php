824cb83320b172b83a9a958599d728532a4c6d49
bayurespati <respatibayu48@gmail.com>
2025-07-10 15:07:53 +0700

    fix typo

--- app/Http/Controllers/PlanController.php
+++ app/Http/Controllers/PlanController.php
@@ -37,7 +37,7 @@ class PlanController extends Controller
                 "is_lop" => $plan->is_lop,
                 "file_prpo" => $plan->file_prpo,
                 "no_prpo" => $plan->no_prpo,
-                'jenis_barang_uuid' => optional($plan->item_variety)->uuid,
+                'jenis_barang_id' => optional($plan->item_variety)->uuid,
                 'tipe_barang_id' => optional($plan->item_type)->uuid,
             ];
         });
